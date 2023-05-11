032bc6d4e4c868db4d221e06131a084bf0fd49b7
lucffernands <lucianoffernands@gmail.com>
2023-05-11 10:47:55 -0300

    GET lists

--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -26,8 +26,8 @@ public class GameService {
 	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll() {
 		List<Game> result = gameRepository.findAll();
-		List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
-		return dto;
+		return result.stream().map(x -> new GameMinDTO(x)).toList();
+		
 	}
 
 }
