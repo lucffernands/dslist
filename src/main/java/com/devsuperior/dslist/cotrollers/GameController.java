d79f39533492f81f6603e2b28b9402ff8b39784f
lucffernands <lucianoffernands@gmail.com>
2023-05-11 11:45:44 -0300

    Embedded id, query SQL, projecion

--- src/main/java/com/devsuperior/dslist/cotrollers/GameController.java
+++ src/main/java/com/devsuperior/dslist/cotrollers/GameController.java
@@ -29,7 +29,7 @@ public class GameController {
 	public List<GameMinDTO> findAll() {
 		List<GameMinDTO> result = gameService.findAll();
 		return result;
-	
 	}
+
 
 }
---
+++ src/main/java/com/devsuperior/dslist/projections/GameMinProjection.java
@@ -0,0 +1,12 @@
+package com.devsuperior.dslist.projections;
+
+public interface GameMinProjection {
+	
+	Long getId();
+	String getTitle();
+	Integer getYaer();
+	String getImgUrl();
+	String getShortDescription();
+	Integer getPosition();
+
+}
